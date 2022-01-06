7f1d52761ef806bf67bbd82a10831dd59b44f604
KetakiDeoskar <88453219+KetakiDeoskar@users.noreply.github.com>
2022-01-06 14:24:39 +0530

    commit_2

--- Entities.sql
+++ Entities.sql
@@ -1,9 +1,10 @@
 /*creating database*/
 create database OnlineFoodDelivery
 
-/*user table*/
 create table Users(
-UserId int identity primary key,
+[ID] [int] IDENTITY(1,1) NOT NULL,
+[PreFix] [varchar](7) default 'UC' NOT NULL,
+[UserId]  AS ([PreFix]+ RIGHT('000' + CAST(Id AS VARCHAR(3)), 3)) PERSISTED primary key,
 FirstName varchar(50) not null,
 LastName varchar(50) not null,
 PhoneNumber varchar(10) not null,
@@ -14,58 +15,64 @@ City varchar(50) not null,
 UserRole bit not null,
 )
 
+insert into Users(FirstName,LastName,PhoneNumber,EmailId,UserPassword,UserAddress,City,UserRole) values('Tina','Smith','9123456789','Tina@gmail.com','Tin@123','sadsfsdgrdfhgfgfxgdsfdf','Pune',0)
+select * from Users
+
 create table Restaurants(
-RestaurantId int identity primary key,
+[ID] [int] IDENTITY(1,1) NOT NULL,
+[PreFix] [varchar](7) default 'RC' NOT NULL,
+[RestaurantId]  AS ([PreFix]+ RIGHT('000' + CAST(Id AS VARCHAR(3)), 3)) PERSISTED primary key,
 RestaurantName varchar(50) not null,
 PhoneNumber varchar(10) not null,
 RestaurantAddress varchar(100) not null,
 City varchar(50) not null,
-UserId int foreign key references Users(UserId)
+UserId varchar(10) foreign key references Users(UserId)
 )
-
+insert into Restaurants(RestaurantName,PhoneNumber,RestaurantAddress,City,UserId) values('Park Inn','987456321','Cannaught place','New Delhi','UC001')
+select * from Restaurants
 
 create table Items(
-ItemId int identity primary key,
-RestaurantId int foreign key references Restaurants(RestaurantId),
+[ID] [int] IDENTITY(1,1) NOT NULL,
+[PreFix] [varchar](7) default 'IC' NOT NULL,
+[ItemId]  AS ([PreFix]+ RIGHT('000' + CAST(Id AS VARCHAR(3)), 3)) PERSISTED primary key,
+RestaurantId varchar(10) foreign key references Restaurants(RestaurantId),
 ItemName varchar(50) not null,
 Price decimal not null,
 ItemDescription varchar(200) not null,
 isAvailable bit not null default 1
 )
 
+insert into Items(RestaurantId,ItemName,Price,ItemDescription) values('RC001','Maggie',40.00,'this is a delicious maggie')
+select * from Items
+
+
 create table DeliveryAgents(
-AgentId int identity primary key,
-RestarauntId int foreign key references Restaurants(RestaurantId),
+[ID] [int] IDENTITY(1,1) NOT NULL,
+[PreFix] [varchar](7) default 'AC' NOT NULL,
+[AgentId]  AS ([PreFix]+ RIGHT('000' + CAST(Id AS VARCHAR(3)), 3)) PERSISTED primary key,
+RestarauntId varchar(10) foreign key references Restaurants(RestaurantId),
 AgentName varchar(30) not null,
 AgentPhone varchar(11) not null)
 
+insert into DeliveryAgents(RestarauntId,AgentName,AgentPhone) values('RC001','Rev','9678991234')
+select * from DeliveryAgents
+
 create table Orders(
-OrderId  int identity primary key,
-RestarauntId int foreign key references Restaurants(RestaurantId),
-AgentId int foreign key references DeliveryAgents(AgentId),
-UserId int foreign key references Users(UserId),
+[ID] [int] IDENTITY(1,1) NOT NULL,
+[PreFix] [varchar](7) default 'OC' NOT NULL,
+[OrderId]  AS ([PreFix]+ RIGHT('000' + CAST(Id AS VARCHAR(3)), 3)) PERSISTED primary key,
+RestarauntId varchar(10) foreign key references Restaurants(RestaurantId),
+AgentId varchar(10) foreign key references DeliveryAgents(AgentId),
+UserId varchar(10) foreign key references Users(UserId),
 PaymentMode varchar(20) not null,
 Quantity int default 1,
 TotalPrice decimal not null,
 OrderStatus varchar(20) not null
 )
 
-insert into Users values('Tina','Smith','9123456789','Tina@gmail.com','Tin@123','sadsfsdgrdfhgfgfxgdsfdf','Pune',0)
-insert into Users values('Priya','Smith','9123456789','Priya@gmail.com','Priy@123','sadsfsdgrdfhgfgfxgdsfdf','Pune',1)
+insert into Orders(RestarauntId,AgentId,UserId,PaymentMode,Quantity,TotalPrice,OrderStatus) values('RC001','AC001','UC001','NetBanking',2,40.00,'Delivered')
+select * from Orders
 
-insert into Restaurants values('Park Inn','987456321','Cannaught place','New Delhi',1)
-insert into Restaurants values('Central Park','907456321','Cannaught place','New Delhi',1)
-
-
-
-insert into Items values(1,'Maggie',40.00,'this is a delicious maggie',0)
-
-
-insert into DeliveryAgents values(1,'Rev','9678991234')
-insert into DeliveryAgents values(2,'Ramesh','8956859658')
-
-
-insert into Orders values(1,1,2,'NetBanking',2,40.00,'Delivered')
 select * from Users
 select *from Restaurants
 select * from Items
